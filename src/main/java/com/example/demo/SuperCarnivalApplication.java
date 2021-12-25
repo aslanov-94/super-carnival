afec32ac36cc68af58ff357c7ae39bbd23e4f4d2
Mustafa Aslanov <mustafa260794@hotmail.com>
2021-12-25 13:16:11 +0400

    first commit

---
+++ src/main/java/com/example/demo/SuperCarnivalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperCarnivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperCarnivalApplication.class, args);
+	}
+
+}
