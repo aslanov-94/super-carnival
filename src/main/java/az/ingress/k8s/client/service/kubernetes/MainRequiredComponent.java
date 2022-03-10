c367763ba225eb794052c82f7790ce64bf109d69
Mustafa Aslanov <mustafa260794@hotmail.com>
2022-03-10 21:28:52 +0400

    Main mapping interface created
    Interface created for main mapping function and default namespace. It could split into different interfaces in the future for Single Responsibility.

---
+++ src/main/java/az/ingress/k8s/client/service/kubernetes/MainRequiredComponent.java
@@ -0,0 +1,60 @@
+package az.ingress.k8s.client.service.kubernetes;
+
+import az.ingress.k8s.client.dto.K8sResourceOwnerDto;
+import az.ingress.k8s.client.dto.KubernetesResourceDto;
+import io.kubernetes.client.common.KubernetesObject;
+import io.kubernetes.client.openapi.models.V1ObjectMeta;
+import io.kubernetes.client.openapi.models.V1OwnerReference;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public interface MainRequiredComponent {
+
+    String DEFAULT_NS = "default";
+
+    default Map<String, KubernetesResourceDto> mapK8sObjectListToResourceMap(List<? extends KubernetesObject> items) {
+        return items.stream()
+                .map(this::mapKubernetesObjectToResourceDto)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors
+                        .toMap(KubernetesResourceDto::getName,
+                                kubernetesResourceDto -> kubernetesResourceDto));
+    }
+
+    private Optional<KubernetesResourceDto> mapKubernetesObjectToResourceDto(KubernetesObject kubernetesObject) {
+        if (Objects.isNull(kubernetesObject.getMetadata())) {
+            return Optional.empty();
+        }
+
+        V1ObjectMeta metadata = kubernetesObject.getMetadata();
+        return Optional.of(
+                KubernetesResourceDto.builder()
+                        .namespace(metadata.getNamespace())
+                        .name(metadata.getName())
+                        .labels(metadata.getLabels())
+                        .kind(kubernetesObject.getKind())
+                        .resourceOwners(mapOwnerReferenceToDto(metadata.getOwnerReferences()))
+                        .build()
+        );
+    }
+
+    private List<K8sResourceOwnerDto> mapOwnerReferenceToDto(List<V1OwnerReference> ownerReferences) {
+        if (Objects.isNull(ownerReferences)) {
+            return List.of();
+        }
+        return ownerReferences.stream()
+                .filter(Objects::nonNull)
+                .map(reference -> K8sResourceOwnerDto.builder()
+                        .apiVersion(reference.getApiVersion())
+                        .kind(reference.getKind())
+                        .name(reference.getName())
+                        .build()
+                )
+                .collect(Collectors.toList());
+    }
+}
